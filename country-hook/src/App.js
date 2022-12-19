735ff4089ca7b4fc0ea0a09ff57c7faec7c3aae9
Anwar Sh <alvaro.sh03@gmail.com>
2022-12-19 21:59:58 +0500

    Implement useCountry custom hook

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -18,7 +18,17 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (name) {
+      axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then(response => response.data[0])
+      .then(data => setCountry(data))
+      .catch(error => {
+        console.log(error.message)
+        setCountry(null)
+      })
+    }
+  }, [name])
 
   return country
 }
@@ -31,11 +41,11 @@ const Country = ({ country }) => {
   return (
     <div>
       <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
+      <div>population {country.population}</div>
       <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`} />
     </div>
-  )  
+  )
 }
 
 const App = () => {
