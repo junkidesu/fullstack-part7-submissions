cff164814dfb419add8f7ac2d24c54aca3a2a4b4
Anwar Sh <alvaro.sh03@gmail.com>
2022-12-24 03:01:31 +0500

    Implement single User component

---
+++ bloglist-frontend/src/components/User.js
@@ -0,0 +1,17 @@
+const User = ({ user }) => {
+  if (!user) return null
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <h3>added blogs</h3>
+      <ul>
+        {user.blogs.map((b) => (
+          <li key={b.id}>{b.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default User
