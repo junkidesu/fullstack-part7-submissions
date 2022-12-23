0ec1ac01ab5d25b6e199ea9ce1e580569907ef71
Anwar Sh <alvaro.sh03@gmail.com>
2022-12-24 02:13:01 +0500

    Implement BlogList component

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { setNotification } from './reducers/notificationReducer'
-import { initializeBlogs, likeBlog, deleteBlog } from './reducers/blogReducer'
+import { initializeBlogs } from './reducers/blogReducer'
 import { loadUsers } from './reducers/usersReducer'
 import { loginUser, logoutUser, restoreUser } from './reducers/userReducer'
-import Blog from './components/Blog'
+import BlogList from './components/BlogList'
 import Togglable from './components/Togglable'
 import Notification from './components/Notification'
 import CreateBlogForm from './components/CreateBlogForm'
@@ -14,7 +13,7 @@ const App = () => {
   const [password, setPassword] = useState('')
 
   const user = useSelector(({ user }) => user)
-  const blogs = useSelector(({ blogs }) => blogs)
+
   const users = useSelector(({ users }) => users)
 
   const dispatch = useDispatch()
@@ -43,23 +42,6 @@ const App = () => {
     console.log('logging out')
 
     dispatch(logoutUser())
-  }
-
-  const like = async (id) => {
-    const blog = blogs.find((b) => b.id === id)
-    dispatch(likeBlog(blog))
-    dispatch(setNotification(`liked blog '${blog.title}'`, 5))
-  }
-
-  const remove = async (id) => {
-    console.log('delete', id)
-
-    const blog = blogs.find((b) => b.id === id)
-
-    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
-      dispatch(deleteBlog(id))
-      dispatch(setNotification(`deleted '${blog.title}'`, 5))
-    }
   }
 
   if (!user) {
@@ -108,17 +90,7 @@ const App = () => {
         <CreateBlogForm />
       </Togglable>
 
-      <div id="blogs">
-        {blogs.map((blog) => (
-          <Blog
-            key={blog.id}
-            blog={blog}
-            like={() => like(blog.id)}
-            deleteBlog={() => remove(blog.id)}
-            username={user.username}
-          />
-        ))}
-      </div>
+      <BlogList />
     </div>
   )
 }
---
+++ bloglist-frontend/src/components/BlogList.js
@@ -0,0 +1,43 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { likeBlog, deleteBlog } from '../reducers/blogReducer'
+import { setNotification } from '../reducers/notificationReducer'
+import Blog from './Blog'
+
+const BlogList = () => {
+  const dispatch = useDispatch()
+  const blogs = useSelector(({ blogs }) => blogs)
+  const user = useSelector(({ user }) => user)
+
+  const like = async (id) => {
+    const blog = blogs.find((b) => b.id === id)
+    dispatch(likeBlog(blog))
+    dispatch(setNotification(`liked blog '${blog.title}'`, 5))
+  }
+
+  const remove = async (id) => {
+    console.log('delete', id)
+
+    const blog = blogs.find((b) => b.id === id)
+
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
+      dispatch(deleteBlog(id))
+      dispatch(setNotification(`deleted '${blog.title}'`, 5))
+    }
+  }
+
+  return (
+    <div id="blogs">
+      {blogs.map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          like={() => like(blog.id)}
+          deleteBlog={() => remove(blog.id)}
+          username={user.username}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default BlogList
