2c0a65ce983d5ee159fbd6b371a2c02f79e0a088
Anwar Sh <alvaro.sh03@gmail.com>
2022-12-23 22:59:06 +0500

    Add setUser and removeUser actions

--- bloglist-frontend/src/reducers/userReducer.js
+++ bloglist-frontend/src/reducers/userReducer.js
@@ -3,6 +3,17 @@ import { createSlice } from '@reduxjs/toolkit'
 const userSlice = createSlice({
   name: 'user',
   initialState: null,
+  reducers: {
+    setUser(state, action) {
+      return action.payload
+    },
+    // eslint-disable-next-line no-unused-vars
+    removeUser(state, action) {
+      return null
+    },
+  },
 })
+
+export const { setUser, removeUser } = userSlice.actions
 
 export default userSlice.reducer
