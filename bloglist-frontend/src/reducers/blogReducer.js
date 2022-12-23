ea1e6a6b70df37bae195216b1eb2b53bf3d7f6b8
Anwar Sh <alvaro.sh03@gmail.com>
2022-12-23 20:59:01 +0500

    Implement initializeBlogs action creator

--- bloglist-frontend/src/reducers/blogReducer.js
+++ bloglist-frontend/src/reducers/blogReducer.js
@@ -1,10 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit'
+import blogServices from '../services/blogs'
 
 const blogSlice = createSlice({
   name: 'blogs',
   initialState: [],
+  reducers: {
+    setBlogs(state, action) {
+      return action.payload
+    },
+  },
 })
 
+export const { setBlogs } = blogSlice.actions
 
+export const initializeBlogs = () => async (dispatch) => {
+  const blogs = await blogServices.getAll()
+  dispatch(setBlogs(blogs))
+}
 
 export default blogSlice.reducer
