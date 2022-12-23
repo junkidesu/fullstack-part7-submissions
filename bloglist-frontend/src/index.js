ae52d059ed5ca40a29c9d1858ab8ef05c36b54d4
Anwar Sh <alvaro.sh03@gmail.com>
2022-12-23 20:26:06 +0500

    Extract Redux store into its own file

--- bloglist-frontend/src/index.js
+++ bloglist-frontend/src/index.js
@@ -2,14 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
-import notificationReducer from './reducers/notificationReducer'
-
-const store = configureStore({
-  reducer: {
-    notification: notificationReducer,
-  },
-})
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
