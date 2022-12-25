faf6a95256848a4b26f092b62b622e9d035f7bb8
Anwar Sh <alvaro.sh03@gmail.com>
2022-12-25 15:08:58 +0500

    Implement individual blog view

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { initializeBlogs } from './reducers/blogReducer'
+import { initializeBlogs, likeBlog, deleteBlog } from './reducers/blogReducer'
 import { loadUsers } from './reducers/usersReducer'
 import { loginUser, logoutUser, restoreUser } from './reducers/userReducer'
+import { setNotification } from './reducers/notificationReducer'
 import { Routes, Route, Link, useMatch } from 'react-router-dom'
 import BlogList from './components/BlogList'
+import Blog from './components/Blog'
 import Users from './components/Users'
 import User from './components/User'
 import Togglable from './components/Togglable'
@@ -26,10 +28,16 @@ const App = () => {
   const [password, setPassword] = useState('')
 
   const user = useSelector(({ user }) => user)
+  const blogs = useSelector(({ blogs }) => blogs)
   const users = useSelector(({ users }) => users)
 
   const match = useMatch('/users/:id')
   const foundUser = match ? users.find((u) => u.id === match.params.id) : null
+
+  const blogMatch = useMatch('/blogs/:id')
+  const foundBlog = blogMatch
+    ? blogs.find((b) => b.id === blogMatch.params.id)
+    : null
 
   const dispatch = useDispatch()
 
@@ -57,6 +65,23 @@ const App = () => {
     console.log('logging out')
 
     dispatch(logoutUser())
+  }
+
+  const like = async (id) => {
+    const blog = blogs.find((b) => b.id === id)
+    dispatch(likeBlog(blog))
+    dispatch(setNotification(`liked blog '${blog.title}'`, 5))
+  }
+
+  const remove = async (id) => {
+    console.log('delete', id)
+
+    const blog = blogs.find((b) => b.id === id)
+
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
+      dispatch(deleteBlog(id))
+      dispatch(setNotification(`deleted '${blog.title}'`, 5))
+    }
   }
 
   if (!user) {
@@ -105,6 +130,17 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
         <Route path="/users/:id" element={<User user={foundUser} />} />
+        <Route
+          path="/blogs/:id"
+          element={
+            <Blog
+              blog={foundBlog}
+              deleteBlog={() => remove(foundBlog.id)}
+              like={() => like(foundBlog.id)}
+              username={user.username}
+            />
+          }
+        />
       </Routes>
     </div>
   )
--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,49 +1,37 @@
-import { useState } from 'react'
-import PropTypes from 'prop-types'
-
 const Blog = ({ blog, like, deleteBlog, username }) => {
-  const [full, setFull] = useState(false)
+  // const [full, setFull] = useState(false)
 
-  const blogStyle = {
-    borderWidth: '1px',
-    borderStyle: 'solid',
-    borderColor: 'black',
-    borderRadius: '5px',
-    paddingTop: '10px',
-    paddingLeft: '2px',
-    marginTop: '5px',
-    marginBottom: '5px',
-  }
+  // const blogStyle = {
+  //   borderWidth: '1px',
+  //   borderStyle: 'solid',
+  //   borderColor: 'black',
+  //   borderRadius: '5px',
+  //   paddingTop: '10px',
+  //   paddingLeft: '2px',
+  //   marginTop: '5px',
+  //   marginBottom: '5px',
+  // }
 
-  const detailsStyle = {
-    display: full ? '' : 'none',
-  }
+  // const detailsStyle = {
+  //   display: full ? '' : 'none',
+  // }
+  if (!blog) return null
 
   return (
-    <div style={blogStyle} className="blog">
-      <div>
+    <div>
+      <h2>
         {blog.title} {blog.author}
-        <button onClick={() => setFull(!full)}>{full ? 'hide' : 'view'}</button>
+      </h2>
+      <a href={blog.url}>{blog.url}</a>
+      <div>
+        {blog.likes} likes <button onClick={like}>like</button>
       </div>
-
-      <div style={detailsStyle} className="blogDetails">
-        <div>{blog.url}</div>
-        <div>{blog.likes}</div>
-        <button onClick={like}>like</button> <br />
-        <div>{blog.user.name}</div>
-        {username === blog.user.username ? (
-          <button onClick={deleteBlog}>remove</button>
-        ) : null}
-      </div>
+      <div>added by {blog.user.name}</div>
+      {username === blog.user.username ? (
+        <button onClick={deleteBlog}>remove</button>
+      ) : null}
     </div>
   )
-}
-
-Blog.propTypes = {
-  blog: PropTypes.object.isRequired,
-  like: PropTypes.func.isRequired,
-  deleteBlog: PropTypes.func.isRequired,
-  username: PropTypes.string.isRequired,
 }
 
 export default Blog
