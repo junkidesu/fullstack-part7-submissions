eaac3fe06feb53773d0d36879ae55c5c10de833b
Anwar Sh <alvaro.sh03@gmail.com>
2022-12-25 15:25:18 +0500

    Items in BlogList link to individual blogs

--- bloglist-frontend/src/components/BlogList.js
+++ bloglist-frontend/src/components/BlogList.js
@@ -1,40 +1,23 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { likeBlog, deleteBlog } from '../reducers/blogReducer'
-import { setNotification } from '../reducers/notificationReducer'
-import Blog from './Blog'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const BlogList = () => {
-  const dispatch = useDispatch()
   const blogs = useSelector(({ blogs }) => blogs)
-  const user = useSelector(({ user }) => user)
 
-  const like = async (id) => {
-    const blog = blogs.find((b) => b.id === id)
-    dispatch(likeBlog(blog))
-    dispatch(setNotification(`liked blog '${blog.title}'`, 5))
-  }
-
-  const remove = async (id) => {
-    console.log('delete', id)
-
-    const blog = blogs.find((b) => b.id === id)
-
-    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
-      dispatch(deleteBlog(id))
-      dispatch(setNotification(`deleted '${blog.title}'`, 5))
-    }
+  const style = {
+    borderStyle: 'solid',
+    borderWidth: '2px',
+    borderColor: 'darkgrey',
+    padding: '10px 5px',
+    margin: '5px 0px',
   }
 
   return (
     <div id="blogs">
       {blogs.map((blog) => (
-        <Blog
-          key={blog.id}
-          blog={blog}
-          likeBlog={() => like(blog.id)}
-          deleteBlog={() => remove(blog.id)}
-          username={user.username}
-        />
+        <div key={blog.id} style={style}>
+          <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+        </div>
       ))}
     </div>
   )
