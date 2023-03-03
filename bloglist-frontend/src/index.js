69a46e53bcef5baf3060f0a919df61aae96e54a5
Anwar Sh <alvaro.sh03@gmail.com>
2023-03-03 20:10:37 +0500

    Install Roboto font

--- bloglist-frontend/src/index.js
+++ bloglist-frontend/src/index.js
@@ -4,6 +4,10 @@ import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
