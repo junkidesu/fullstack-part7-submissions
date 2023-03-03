583a1cca1d228f3f1a118e6cdc021184d3516f54
Anwar Sh <alvaro.sh03@gmail.com>
2023-03-03 17:35:39 +0500

    Individual user view uses material tables

--- bloglist-frontend/src/components/User.js
+++ bloglist-frontend/src/components/User.js
@@ -1,19 +1,29 @@
 import { Link } from 'react-router-dom'
-
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material'
 const User = ({ user }) => {
   if (!user) return null
 
   return (
     <div>
       <h2>{user.name}</h2>
-      <h3>added blogs</h3>
-      <ul>
-        {user.blogs.map((b) => (
-          <li key={b.id}>
-            <Link to={`/blogs/${b.id}`}>{b.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell><b>Added Blogs</b></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {user.blogs.map((b) => (
+              <TableRow key={b.id}>
+                <TableCell>
+                  <Link to={`/blogs/${b.id}`}>{b.title}</Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
